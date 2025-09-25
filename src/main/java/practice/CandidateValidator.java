2e5ef7cc7bc6286fef29dea1b82d45d6f9fcd5a5
Vo7kov <jegor.volkov.a@gmail.com>
2025-09-25 13:48:29 +0200

    feat(DEVELOP): stream API practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate c) {
+        if (c.getAge() < 35) {
+            return false;
+        }
+
+        if (!c.isAllowedToVote()) {
+            return false;
+        }
+
+        if (!"Ukrainian".equals(c.getNationality())) {
+            return false;
+        }
+
+        String[] years = c.getPeriodsInUkr().split("-");
+        int from = Integer.parseInt(years[0]);
+        int to = Integer.parseInt(years[1]);
+
+        return (to - from + 1) >= 10;
+    }
 }
